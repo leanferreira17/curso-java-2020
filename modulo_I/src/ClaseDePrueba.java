a3c4ac7cb066d5b3fbf556ab9fa77421186b9bb8
Leandro Ivan Ferreira <liferreira@vates.com>
2020-07-21 19:14:53 -0300

    Adding material to class 1

---
+++ modulo_I/src/ClaseDePrueba.java
@@ -0,0 +1,38 @@
+class ClaseDePrueba {
+    public static void main(String[] args) {
+
+        /*
+        * Tarea: jugar con los tipos primitivos de la tabla y con los operadores aritméticos
+        * */
+
+        // Tipos primitivos
+
+        // Para recordar. Convención:
+        // Usar camelCase para nombrar las variables
+
+        // Fórmula mágica para usar un tipo primitivo (no se olviden de cerrar la declaración con el punto y coma ;)
+        // [tipo primitivo] + [nombreDeLaVariable] = [valor];
+
+        // Declaración de una variable
+        float valorDeLaVariable;
+
+        // Inicialización de la variable (operador de asignación =)
+        valorDeLaVariable = 34.45f;
+
+        // Declaración e inicialización todo de una
+        long valorDeLaSegundaVariable = 23454324523453L;
+
+        // Operadores aritméticos
+        // Suma +
+        // Resta -
+        // Multiplicación *
+        // división /
+        // Resto %
+
+        // Este método "System.out.println()" nos permite imprimir por consola el resultado (lo veremos el jueves)
+        System.out.println(valorDeLaVariable + valorDeLaSegundaVariable);
+
+        // Practiquen mucho, jueguen y rompan todo que no pasa nada!
+        // Cualquier consulta a mi email. Saludos.
+    }
+}
